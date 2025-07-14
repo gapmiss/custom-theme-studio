1ac3a3fa6498d673f106c9af88a9b9c63fbd613e
gapmiss <98914514+gapmiss@users.noreply.github.com>
2025-07-13 19:09:12 -0500

    enableAceColorPicker default false

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -73,7 +73,7 @@ export const DEFAULT_SETTINGS: CustomThemeStudioSettings = {
 	enableColorPicker: false,
 	enableAceAutoCompletion: false,
 	enableAceSnippets: false,
-	enableAceColorPicker: true,
+	enableAceColorPicker: false,
 	editorLineNumbers: true,
 	editorWordWrap: false,
 	editorFontSize: 15,
